8194549cf71780407f7990bebdae2e168341f695
Thomas-Aguirre <166098588+Thomas-Aguirre@users.noreply.github.com>
2024-04-20 21:43:31 +0000

    El tp0 finalizado

--- client/src/client.c
+++ client/src/client.c
@@ -1,5 +1,5 @@
 #include "client.h"
-
+ 
 int main(void)
 {
 	/*---------------------------------------------------PARTE 2-------------------------------------------------------------*/
@@ -15,10 +15,10 @@ int main(void)
 	/* ---------------- LOGGING ---------------- */
 
 	logger = iniciar_logger();
-
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
-
+     log_info(logger,"Hola! Soy un log");
+	 
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
@@ -26,9 +26,13 @@ int main(void)
 
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
-
+     valor=config_get_string_value(config,"CLAVE");
+	 ip=config_get_string_value(config,"IP");
+	 puerto=config_get_string_value(config,"PUERTO");
 	// Loggeamos el valor de config
-
+      log_info(logger,"La clave de la config: %s", valor);
+	  log_info(logger,"El puerto de la config: %s",puerto);
+	  log_info(logger,"La ip de la config: %s",ip);
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
@@ -36,13 +40,13 @@ int main(void)
 
 	/*---------------------------------------------------PARTE 3-------------------------------------------------------------*/
 
-	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
-
+	/* ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor
+	 esté corriendo para poder conectarnos a él*/
 	// Creamos una conexión hacia el servidor
 	conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
-
+       enviar_mensaje(valor,conexion);
 	// Armamos y enviamos el paquete
 	paquete(conexion);
 
@@ -55,29 +59,41 @@ int main(void)
 t_log* iniciar_logger(void)
 {
 	t_log* nuevo_logger;
-
+    nuevo_logger= log_create("tp0.log","codigo",true,LOG_LEVEL_INFO);
+	if(nuevo_logger == NULL){
+        //si nuevo_logger me devuelve un NULL significa que algo raro paso con el log
+		perror("No se pudo encontrar o crear el archivo");
+		exit(EXIT_FAILURE);
+	};
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
 	t_config* nuevo_config;
-
+    nuevo_config=config_create("cliente.config");
+	if(nuevo_config == NULL){
+        perror("Hubo un error al intentar cargar el config");
+		exit(EXIT_FAILURE);
+	};
 	return nuevo_config;
 }
 
 void leer_consola(t_log* logger)
 {
 	char* leido;
-
-	// La primera te la dejo de yapa
-	leido = readline("> ");
-
+	// La primera te la dejo de yapa 
+	printf("Escribi lo necesario \n");
+	leido = readline("> "); //duda, no veo lo que escribo en consola ??????????????
 	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
-
-
+	 while(string_is_empty(leido) == false){ //uso de string_is_empty, si ve que la variable leido 
+		log_info(logger, ">> %s",leido);     // no es vacio entonces devuelve false
+		free(leido);
+        leido = readline("> ");
+	 };
 	// ¡No te olvides de liberar las lineas antes de regresar!
-
+    free(leido);
+	
 }
 
 void paquete(int conexion)
@@ -85,16 +101,27 @@ void paquete(int conexion)
 	// Ahora toca lo divertido!
 	char* leido;
 	t_paquete* paquete;
-
+    paquete=crear_paquete();
 	// Leemos y esta vez agregamos las lineas al paquete
-
-
+    leido = readline("> "); 
+    while(string_is_empty(leido) == false){ 
+		agregar_a_paquete(paquete,leido,strlen(leido)+1);
+		free(leido);
+        leido = readline("> ");
+	};
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
-	
+	free(leido);
+	//ENVIAR PAQUETE	
+	enviar_paquete(paquete,conexion);
+	//ELIMINAR PAQUETE	
+	eliminar_paquete(paquete);
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	  log_destroy(logger);
+	  config_destroy(config);
+	  liberar_conexion(conexion);
 }
